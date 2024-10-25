f806aba7dae7d76e213ba9fa0bfabf3f39fb56af
Carlos Moreira <cesmoreira90@hotmail.com>
2024-10-25 17:19:30 -0300

    merge with new admin sidebar

--- src/components/AdminSidebar.tsx
+++ src/components/AdminSidebar.tsx
@@ -15,80 +15,80 @@ export function AdminSidebar() {
   }
 
   return (
-    <SidebarProvider>
-      <Sidebar className="bg-oxfordBlue text-white">
-        <SidebarHeader>
-          {/* Substitua pelo seu logo */}
-          <img src={logoClintia} alt="Clintia" className="w-full h-auto" />
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="#inicio">
-                    <Home className="inline-block mr-2" /> Início
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="#exames">
-                    <FileText className="inline-block mr-2" /> Exames
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="#agendamentos">
-                    <Calendar className="inline-block mr-2" /> Agendamentos
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="#financeiro">
-                    <DollarSign className="inline-block mr-2" /> Financeiro
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="#pacientes">
-                    <Users className="inline-block mr-2" /> Pacientes
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <a href="#exames-clinica">
-                    <FilePlus className="inline-block mr-2" /> Exames da clínica
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-          <div className="flex justify-between items-center">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <button>
-                  <User className="w-6 h-6" />
-                </button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent side="top">
-                <DropdownMenuItem>
-                  <span>Editar Perfil</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <button onClick={handleLogout}>
-              <LogOut className="w-6 h-6" />
-            </button>
-          </div>
-        </SidebarFooter>
-      </Sidebar>
-    </SidebarProvider>
+      <SidebarProvider>
+        <Sidebar className="bg-oxfordBlue text-white">
+          <SidebarHeader>
+            {/* Substitua pelo seu logo */}
+            <img src={logoClintia} alt="Clintia" className="w-full h-auto" />
+          </SidebarHeader>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <a href="#inicio">
+                      <Home className="inline-block mr-2" /> Início
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <a href="#exames">
+                      <FileText className="inline-block mr-2" /> Exames
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <a href="#agendamentos">
+                      <Calendar className="inline-block mr-2" /> Agendamentos
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <a href="#financeiro">
+                      <DollarSign className="inline-block mr-2" /> Financeiro
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <a href="#pacientes">
+                      <Users className="inline-block mr-2" /> Pacientes
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton asChild>
+                    <a href="#exames-clinica">
+                      <FilePlus className="inline-block mr-2" /> Exames da clínica
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarGroup>
+          </SidebarContent>
+          <SidebarFooter>
+            <div className="flex justify-between items-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button>
+                    <User className="w-6 h-6" />
+                  </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="top">
+                  <DropdownMenuItem>
+                    <span>Editar Perfil</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <button onClick={handleLogout}>
+                <LogOut className="w-6 h-6" />
+              </button>
+            </div>
+          </SidebarFooter>
+        </Sidebar>
+      </SidebarProvider>
   )
 }
