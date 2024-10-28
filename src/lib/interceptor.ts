874e3563a7e4009c6541ef44e47b691dff869a45
Carlos Moreira <cesmoreira90@hotmail.com>
2024-10-28 14:13:07 -0300

    update endpoint

--- src/lib/interceptor.ts
+++ src/lib/interceptor.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const apiClient = axios.create({
-    baseURL: 'https://api-pre.clintia.com.br',
+    baseURL: 'https://api.clintia.com.br',
 });
 
 // Interceptor de requisição
